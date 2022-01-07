2716538a9597b2299dcfd4444e41bb70bb188584
tp <t-poulsen@hotmail.com>
2022-01-07 01:07:19 -0600

    testing simpler version

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 timeBot.py
+web: python3 test.py
--- test.py
+++ test.py
@@ -1,18 +1,62 @@
-# from messageParser import calculate_time
-# 
-# 
-# print(calculate_time("11:05am", "5:15pm", 1.5, 3.5))
-# print(calculate_time("12:05am", "5:15pm", 1.5, 3.5))
-# 
-# 
-# print(f"\nstring to float:")
-# bob = "bob"
-# print("string: " + bob)
-# try:
-#     bobf = float(bob)
-# except:
-#     bobf = "invalid draw amount"
-# finally:
-#     print(bobf)
+import json
+import os
+from twilio.rest import Client
+from datetime import date
+
+from fastapi import FastAPI, Request, Form
+from pydantic import BaseModel
+
+from twilio.twiml.messaging_response import MessagingResponse
+
+# import databaseAccess
+import exceptions
+import messageParser
+
+number = os.environ['test_phone']
+account = os.environ['account_sid']
+token = os.environ['auth_token']
+
+client = Client(account, token)
+
+app = FastAPI()
+
+users = {
+    "+12083500006": "Taylor Poulsen",
+    "+14322761331": "Daniel Bogden"
+}
 
 
+@app.post("/sms")
+async def response(From: str = Form(...), Body: str = Form(...)) -> str:
+    # print(f"Mesage from: {From}")
+    # print(f"Body: {Body}")
+
+
+    try:
+        # body = message
+        result = messageParser.process_message(Body)
+        print(result)   
+    except exceptions.DrawException:
+        # body = "Draw formatted incorrectly" 
+        print("Draw formatted incorrectly")
+    except Exception as e:
+        print("there was an exception")
+        print(e.with_traceback)
+
+
+    if not response:
+        mess = client.messages.create(
+            body = "Join Earth's mightiest heroes. Like Kevin Bacon.",
+            from_ = '+18324971734',
+            to = From
+        )
+    else:
+        mess = client.messages.create(
+            body = response,
+            from_ = '+18324971734',
+            to = From
+        )
+
+    # response = MessagingResponse()
+    # response.message(mess)
+    # return response
