36e76d15a8010517a28ecf5014b7b26edf2cf683
tp <t-poulsen@hotmail.com>
2022-01-07 01:14:34 -0600

    even simpler

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 import json
 import os
-from twilio.rest import Client
-from datetime import date
+import twilio
+# from datetime import date
 
 from fastapi import FastAPI, Request, Form
-from pydantic import BaseModel
-
-from twilio.twiml.messaging_response import MessagingResponse
 
 # import databaseAccess
-import exceptions
-import messageParser
+# import exceptions
+# import messageParser
 
 number = os.environ['test_phone']
 account = os.environ['account_sid']
@@ -28,35 +25,35 @@ users = {
 
 @app.post("/sms")
 async def response(From: str = Form(...), Body: str = Form(...)) -> str:
-    # print(f"Mesage from: {From}")
-    # print(f"Body: {Body}")
+    print(f"Mesage from: {From}")
+    print(f"Body: {Body}")
 
 
-    try:
-        # body = message
-        result = messageParser.process_message(Body)
-        print(result)   
-    except exceptions.DrawException:
-        # body = "Draw formatted incorrectly" 
-        print("Draw formatted incorrectly")
-    except Exception as e:
-        print("there was an exception")
-        print(e.with_traceback)
+    # try:
+    #     # body = message
+    #     # result = messageParser.process_message(Body)
+    #     print()   
+    # except exceptions.DrawException:
+    #     # body = "Draw formatted incorrectly" 
+    #     print("Draw formatted incorrectly")
+    # except Exception as e:
+    #     print("there was an exception")
+    #     print(e.with_traceback)
 
 
-    if not response:
-        mess = client.messages.create(
-            body = "Join Earth's mightiest heroes. Like Kevin Bacon.",
-            from_ = '+18324971734',
-            to = From
-        )
-    else:
-        mess = client.messages.create(
-            body = response,
-            from_ = '+18324971734',
-            to = From
-        )
+    # if not response:
+    #     mess = client.messages.create(
+    #         body = "Join Earth's mightiest heroes. Like Kevin Bacon.",
+    #         from_ = '+18324971734',
+    #         to = From
+    #     )
+    # else:
+    #     mess = client.messages.create(
+    #         body = response,
+    #         from_ = '+18324971734',
+    #         to = From
+    #     )
 
-    # response = MessagingResponse()
-    # response.message(mess)
-    # return response
+    # # response = MessagingResponse()
+    # # response.message(mess)
+    # # return response
