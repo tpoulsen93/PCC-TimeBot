5f4e0eb4ae0359c9d74ac953be75575580470f9b
tpoulsen93 <t-poulsen@hotmail.com>
2022-01-13 22:07:10 -0700

    fixed some bugs and added more flexibility to hours format -> 8am instead of 8:00am (2)

--- src/timeCalc.py
+++ src/timeCalc.py
@@ -1,4 +1,4 @@
-import src.exceptions as exceptions
+from src.exceptions import *
 from datetime import timedelta
 
 
@@ -6,16 +6,16 @@ from datetime import timedelta
 def calculate_time(start: str, end: str, less: str, more: str) -> float:
     # length of times should be 6 or 7  -->  00:00xm or 0:00xm
     if len(start) < 3 or len(start) > 7 or len(end) < 3 or len(end) > 7:
-        raise exceptions.TimeFormatException
+        raise TimeFormatException
 
     # build clock-in time
-    if len(start > 4):
+    if len(start > 4):  # 9:00am
         arr = start.split(":")
 
         # validate the start hours and meridiem
         startHours = int(arr[0])
         if startHours < 1 or startHours > 12:
-            raise exceptions.HoursException
+            raise HoursException
 
         if arr[1].endswith("am"):
             arr[1] = arr[1].replace("am", "")
@@ -23,38 +23,39 @@ def calculate_time(start: str, end: str, less: str, more: str) -> float:
             startHours += 12
             arr[1] = arr[1].replace("pm", "")
         else: # neither am nor pm detected
-            raise exceptions.MeridiemException
+            raise MeridiemException
 
         # validate the start minutes
         startMinutes = int(arr[1])
         if startMinutes < 0 or startMinutes > 59:
-            raise exceptions.MinutesException
+            raise MinutesException
 
-    else:
-        if start < 1 or start > 12:
-            raise exceptions.HoursException
+    else:   # 9am
+        startMinutes = 0
         if start.endswith("am"):
             start = start.replace("am", "")
+            startHours = int(start)
         elif start.endswith("pm"):
-            start += 12
             start = start.replace("pm", "")
+            startHours = int(start) + 12
         else: # neither am nor pm detected
-            raise exceptions.MeridiemException
-        startHours = start
-        startMinutes = 0
+            raise MeridiemException
+
+        if start < 1 or start > 12:
+            raise HoursException
 
     startTime = timedelta(hours=startHours, minutes=startMinutes)
 
             
 
     # build clock-out time
-    if len(end) > 4:
+    if len(end) > 4:    # 9:00am
         arr = end.split(":")
 
         # validate end hours and meridiem
         endHours = int(arr[0])
         if endHours < 1 or endHours > 12:
-            raise exceptions.HoursException
+            raise HoursException
 
         if arr[1].endswith("am"):
             arr[1] = arr[1].replace("am", "")
@@ -62,25 +63,26 @@ def calculate_time(start: str, end: str, less: str, more: str) -> float:
             endHours += 12
             arr[1] = arr[1].replace("pm", "")
         else: # neither am nor pm detected
-            raise exceptions.MeridiemException
+            raise MeridiemException
 
         # validate end minutes
         endMinutes = int(arr[1])
         if endMinutes < 0 or endMinutes > 59:
-            raise exceptions.MinutesException
+            raise MinutesException
 
-    else:
-        if end < 1 or end > 12:
-            raise exceptions.HoursException
+    else:   # 9am
+        endMinutes = 0
         if end.endswith("am"):
             end = end.replace("am", "")
+            endHours = int(end)
         elif end.endswith("pm"):
-            end += 12
             end = end.replace("pm", "")
+            endHours = int(end) + 12
         else: # neither am nor pm detected
-            raise exceptions.MeridiemException
-        endHours = end
-        endMinutes = 0
+            raise MeridiemException
+
+        if endHours < 1 or endHours > 12:
+            raise HoursException
     
     endTime = timedelta(hours=endHours, minutes=endMinutes)
 
@@ -88,17 +90,17 @@ def calculate_time(start: str, end: str, less: str, more: str) -> float:
 
     # check for any other exceptions in the message
     if endTime < startTime:
-        raise exceptions.IllegalTimeException
+        raise IllegalTimeException
     
     try:
         subtract = float(less)
     except:
-        raise exceptions.LunchException
+        raise LunchException
 
     try:
         add = float(more) if more != "" else 0
     except:
-        raise exceptions.ExtraException
+        raise ExtraException
 
     # compute hours for the day and return as a float rounded to 2 decimal places
     hours = endTime - startTime - timedelta(hours=subtract) + timedelta(hours=add)
