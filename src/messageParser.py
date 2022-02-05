cdf63e87206268b89298a3b17bd9b5c4ea964838
tpoulsen93 <t-poulsen@hotmail.com>
2022-02-04 18:59:44 -0700

    updated confirmation texts 2

--- src/messageParser.py
+++ src/messageParser.py
@@ -54,7 +54,7 @@ def process_time(message: str, fro: str) -> str:
     submission = da.submit_time(employee_id, time, message)
     if more == "":
         more = 0
-    result =  f"{today}\n{mess[1].title()} {mess[2].title()} {submission}"
+    result =  f"{today}\n{mess[1].title()} {mess[2].title()} {submission}\n"
     result += f"Subtracted hours: {less}\nAdded hours: {more}"
     confirmation = ta.confirm_submission(employee_id, result, fro)
 
