d05359d7e2c22ab3745b5dafd5dc61483cc45e73
tpoulsen93 <t-poulsen@hotmail.com>
2022-03-23 14:26:40 -0600

    No more accidental duplicate submissions.

--- src/databaseAccess.py
+++ src/databaseAccess.py
@@ -112,13 +112,6 @@ def submit_time(id, time, msg) -> str:
     stmt = text(f"INSERT INTO payroll(id, time, date, message) VALUES(:i, :t, :d, :m) \
         ON CONFLICT ON CONSTRAINT submission DO UPDATE SET time = EXCLUDED.time, message = EXCLUDED.message")
 
-    # dupe = duplicate_submission(id, today)
-    # if not dupe:
-    #     stmt = insert(payroll).values(id = id, time = time, date = today, message = msg)
-    #     result = f"Submitted hours: {time:g}"
-    # else:
-    #     stmt = text("UPDATE payroll SET time = :t, message = :m WHERE id = :i AND date = :d")
-    #     result = f"Updated hours: {dupe:g} to {time:g}"
     with engine.connect() as conn:
         conn.execute(stmt, t = time, m = msg, i = id, d = today)
     return result
