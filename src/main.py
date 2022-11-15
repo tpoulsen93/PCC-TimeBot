0f073a78ca6ae826bbdbc1dec74bc789bd111f9a
tpoulsen93 <t-poulsen@hotmail.com>
2022-11-14 22:52:58 -0700

    added endpoint for submittings hours

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-import src.messageParser as messageParser
-import src.databaseAccess as databaseAccess
-import json, sys, os
+import os
+import sys
 
-from fastapi import FastAPI, Form, Response, Request, HTTPException
+from fastapi import FastAPI, Form, HTTPException, Request, Response
+from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.request_validator import RequestValidator  
+
+import src.databaseAccess as databaseAccess
+import src.messageParser as messageParser
+import src.twilioActions as twilioActions
 
 app = FastAPI()
+
 
 def text_usage(response: MessagingResponse):
     response.message(
@@ -20,6 +24,13 @@ def read_root():
     return {"PCC": "Poulsen Concrete Contractors Inc."}
 
 
+@app.get("/submitHours")
+async def submit_hours(id: int, date: str, hours: float):
+    result = databaseAccess.add_time(id, date, hours)
+    twilioActions.confirm_submission(id, result, "TimeApp")
+    return result
+
+
 @app.post("/sms")
 async def parse_message(request: Request, From: str = Form(...), Body: str = Form(...)):
     # make sure the request is from Twilio not a rando
@@ -27,14 +38,16 @@ async def parse_message(request: Request, From: str = Form(...), Body: str = For
     form_ = await request.form()
     if not validator.validate(str(request.url), form_, request.headers.get("X-Twilio-Signature", "")):
         print("unexpected user encountered")
-        raise HTTPException(status_code=400, detail="Error in Twilio Signature")
+        raise HTTPException(
+            status_code=400, detail="Error in Twilio Signature")
 
     # process the message
-    response = MessagingResponse()     
+    response = MessagingResponse()
     try:
         msg = messageParser.process_message(Body, From)
     except Exception as e:
-        response.message("Encountered an unexpected error. Check your format and try again.")
+        response.message(
+            "Encountered an unexpected error. Check your format and try again.")
         print("Encountered unexpected error in message:")
         print(f"[{Body}]")
         print(e)
@@ -57,7 +70,6 @@ async def parse_message(request: Request, From: str = Form(...), Body: str = For
         print(f"[{Body}]")
         print("Responded:")
         print(f"[{msg}]")
-        
 
     sys.stdout.flush()
     return Response(content=str(response), media_type="application/xml")
