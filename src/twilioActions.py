27020bbb9de0a4a6c66ce527afe9377241e2a93a
tpoulsen93 <t-poulsen@hotmail.com>
2023-09-23 15:46:06 -0600

    updated to not send confirmation texts when admin submits hours

--- src/twilioActions.py
+++ src/twilioActions.py
@@ -1,60 +1,67 @@
 import src.databaseAccess as databaseAccess
 from twilio.rest import Client
+from enum import Enum
 import os
 
+UserType = Enum("UserType", ["Admin", "Owner", "Supervisor", "Employee"])
 
-# send the submission to the supervisor and myself
-def send_text(id: int, msg: str) -> bool:
+
+def send_text(id: int, msg: str):
     phone = databaseAccess.get_employee_phone(id)
     if not phone:
+        return
+
+    client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
+
+    twilio = os.environ["TWILIO_PHONE"]
+    client.messages.create(from_=f"+1{twilio}", to=f"+1{phone}", body=msg)
+
+
+def send_confirmation(id: int, user_type: UserType, is_admin: bool, msg: str):
+    if not id:
+        return f"Error. {user_type.name} ID not found"
+    if id < 1:  # if id < 1, do nothing. supervisor id can be < 1 if it's for an employee with no supervisor
         return False
 
-    client = Client(
-        os.environ['TWILIO_ACCOUNT_SID'],
-        os.environ['TWILIO_AUTH_TOKEN']
-    )
+    phone = databaseAccess.get_employee_phone(id)
+    if not phone:
+        return f"Error. {user_type.name} phone not found"
+    if not is_admin:
+        send_text(id, msg)
+        return False
 
-    twilio = os.environ['TWILIO_PHONE']
-    client.messages.create(
-        from_=f"+1{twilio}",
-        to=f"+1{phone}",
-        body=msg
-    )
-
-    return True
 
 # send confirmation texts of a submission to myself, the supervisor, and the recipient
 def confirm_submission(employee_id: int, msg: str, fro: str):
-    # text confirmation to owner
-    owner_id = databaseAccess.get_employee_id('jr', 'poulsen')
-    if not owner_id:
-        return "Error. Owner not found..."
-    owner_phone = databaseAccess.get_employee_phone(owner_id)
-    if not owner_phone:
-        return "Error. Owner phone not found."
-    send_text(owner_id, msg)
-
-    # text confirmation to supervisor
+    # get relevant id's
+    admin_id = databaseAccess.get_employee_id("admin", "admin")
+    owner_id = databaseAccess.get_employee_id("jr", "poulsen")
     supervisor_id = databaseAccess.get_super_id(employee_id)
-    if supervisor_id != owner_id: # don't text the supervisor twice if it is also the owner
-        if not supervisor_id:
-            return "Error. Supervisor not found."
-        if supervisor_id > 0: # -1 means no supervisor, so text no one
-            supervisor_phone = databaseAccess.get_employee_phone(supervisor_id)
-            if not supervisor_phone:
-                return "Error. Supervisor phone not found."
-            send_text(supervisor_id, msg)
+
+    # don't send confirmations to supervisor or owner if the admin submits hours
+    # admin id is 1 and my employee id is 2. so in either of those cases, we don't want to send confirmations
+    is_admin = employee_id < 3
 
     # text confirmation to admin
-    admin_id = databaseAccess.get_employee_id('admin', 'admin')
-    if not admin_id:
-        return "Error. Admin not found..."
-    admin_phone = databaseAccess.get_employee_phone(admin_id)
-    if not admin_phone:
-        return "Error. Admin phone not found."
-    send_text(admin_id, msg)
+    admin_confirmation = send_confirmation(admin_id, UserType.Admin, is_admin, msg)
+    if admin_confirmation:
+        return admin_confirmation
+
+    # text confirmation to owner
+    owner_confirmation = send_confirmation(owner_id, UserType.Owner, is_admin, msg)
+    if owner_confirmation:
+        return owner_confirmation
+
+    # text confirmation to supervisor
+    supervisor_confirmation = send_confirmation(
+        supervisor_id, UserType.Supervisor, is_admin, msg
+    )
+    if supervisor_confirmation:
+        return supervisor_confirmation
 
     # text confirmation to recipient if they aren't already texted in the response
+    # this is to handle the case that someone submits hours from a phone number other than their usual number.
+    # that way, if someone submits another persons hours when they shouldn't, the affected user gets notified
     phone = databaseAccess.get_employee_phone(employee_id)
     if phone != fro.replace("+1", ""):
         send_text(employee_id, msg)
